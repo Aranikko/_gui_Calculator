00cd8b6dbfdd2109f94e8af09d294341e54dc0b1
Aranikko <129112827+Aranikko@users.noreply.github.com>
2023-09-23 19:00:28 +0300

    0.1
    Create button

--- main.cpp
+++ main.cpp
@@ -1,34 +1,54 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SG");
-    
-    sf::Texture texture;
-    texture.loadFromFile("img/shape_texture.png");
-    texture.setSmooth(true);
+bool Click_check(sf::Vector2i mousePos, sf::FloatRect rectBounds);
 
-    sf::Sprite sprite;
-    sprite.setTexture(texture);
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Calculator");
 
-    while (window.isOpen())
-    {
+    sf::Font font;
+    font.loadFromFile("BebasNeue-Regular.ttf");
+
+    sf::RectangleShape rect(sf::Vector2f(100, 100));
+    rect.setPosition(0, window.getSize().y - rect.getSize().y);
+    rect.setFillColor(sf::Color::Transparent);
+
+    sf::Text text;
+    text.setString("0");
+    text.setFont(font);
+    text.setCharacterSize(50);
+    text.setPosition(
+        rect.getPosition().x + rect.getSize().x / 2 - text.getGlobalBounds().width / 2,
+        rect.getPosition().y + rect.getSize().y / 2 - text.getGlobalBounds().height / 2
+    );
+
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                if (Click_check(mousePos, rect.getGlobalBounds())){
+                    std::cout << "Left button clicked inside the rectangle!" << std::endl;
+                }
+            }
         }
-        window.clear(sf::Color::Black);
 
-        // draw everything here...
-        // window.draw(...);
-
-        window.draw(sprite);
-
-        // end the current frame
+        window.clear();
+        window.draw(rect);
+        window.draw(text);
         window.display();
     }
 
     return 0;
+}
+
+bool Click_check(sf::Vector2i mousePos, sf::FloatRect rectBounds) {
+    if (rectBounds.contains(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y))) {
+        return true;
+    }
+    else{
+        return false;
+    }
 }
